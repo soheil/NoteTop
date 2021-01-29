72a1571a3224d3cc57371a9a79b2dd07786b3595
Soheil <ysoheil@gmail.com>
2021-01-28 19:53:57 -0800

    Initial Commit

---
+++ NoteTop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoteTop
+//
+//  Created by marrynoname on 1/28/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NoteTop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NoteTop
+//
+//  Created by marrynoname on 1/28/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
